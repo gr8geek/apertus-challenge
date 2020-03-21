35f413bb87c4ef51989344e75651b2b61bc165ac
Pratyush Harsh <pratyush9000@gmail.com>
2020-03-21 15:38:00 +0530

    Update test.c

--- characterdevice/test.c
+++ characterdevice/test.c
@@ -7,21 +7,43 @@
 #include <errno.h>
 #include <string.h>
 #include<sys/ioctl.h>
+
 #define WR_VALUE _IOW('a','a',int32_t*)
 #define RD_VALUE _IOR('a','b',int32_t*)
-int main(int argc, char *argv[])
+
+void read_proc()
 {
-	int fd,fd3;
 	long value;
 	char buf[100],buf2[100];
-	int n, c,len;
-	int ret,ct=1;
 	int prc = open("/proc/apertus", O_RDWR);
-	fd3 	= open("/dev/ioctl_device", O_RDWR);
+	printf("Reading From the procfile  :)\n");
+	lseek(prc, 0 , SEEK_SET);
+	read(prc, buf2, 100);
+	puts(buf2);
+	printf("\n");
+	close(prc);
+}
+int read_ioctl()
+{
+	int fd3;
+	long value;
+	fd3 = open("/dev/ioctl_device", O_RDWR);
 	if(fd3 < 0) {
-			printf("Cannot open device file...\n");
-			return 0;
-		}
+		printf("Cannot open device file...\n");
+		return 0;
+	}
+	printf("\nReading Checksum Value from device file using ioctl\n");
+	ioctl(fd3, RD_VALUE, &value);
+	printf("\nValue is %ld\n", value);
+	close(fd3);
+
+}
+void write_chdv(int argc, char *argv[])
+{
+	int fd;
+	char buf[100];
+	int n,c;
+	int ret;
 	if (argc < 2) {
 		fprintf(stderr, "usage: %s <dev>\n", argv[0]);
 		exit(EXIT_FAILURE);
@@ -44,19 +66,14 @@ int main(int argc, char *argv[])
 	}
 	n = ret;
 	printf("wrote %d bytes into file %s\n", n, argv[1]);
-	}	
-	printf("Reading From the procfile  :)\n");
-	lseek(prc, 0 , SEEK_SET);
-	read(prc, buf2, 100);
-	puts(buf2);
-	printf("\n");
-	printf("\nReading Checksum Value from device file using ioctl\n");
-	ioctl(fd3, RD_VALUE, &value);
-	printf("\nValue is %ld\n", value);
-	close(prc);
-	close(fd3);
-	close(fd);
-	return 0;
+	}
+	close(fd);	
 
 }
-
+int main(int argc, char *argv[])
+{
+	write_chdv(argc,argv);
+	read_proc();
+	read_ioctl();
+	return 0;
+}
