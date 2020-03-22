e9f5d1ad33a41572490661bdcb0eada13984f292
Pratyush Harsh <pratyush9000@gmail.com>
2020-03-22 16:02:19 +0530

    Update chardvr.c

--- characterdevice/chardvr.c
+++ characterdevice/chardvr.c
@@ -13,18 +13,18 @@
 #include <linux/slab.h>
 #include <linux/ioctl.h>
 
-#define BUF_LEN 100
-#define WR_VALUE _IOW('a', 'a', int32_t *)
-#define RD_VALUE _IOR('a', 'b', int32_t *)
+#define  BUF_LEN 	100
+#define  WR_VALUE 	_IOW('a', 'a', int32_t *)
+#define  RD_VALUE 	_IOR('a', 'b', int32_t *)
 
-static dev_t dev = 0;
-static int major;
-static char chrdev_buf[BUF_LEN];
-static char buf[BUF_LEN];
-static long checksum = 0;
-static struct proc_dir_entry * ent;
-static struct class * dev_class;
-static struct cdev ioctl_cdev;
+static   dev_t 		dev = 0;
+static   int 		major;
+static   char 		chrdev_buf[BUF_LEN];
+static   char 		buf[BUF_LEN];
+static   long 	      	checksum = 0;
+static   struct 	proc_dir_entry * ent;
+static   struct 	class * dev_class;
+static   struct 	cdev ioctl_cdev;
 
 static ssize_t myread(struct file *file, char __user *ubuf, size_t count, loff_t *ppos)
 {
@@ -37,7 +37,7 @@ static ssize_t myread(struct file *file, char __user *ubuf, size_t count, loff_t
 		return 0;
 
 	for (i = 0; i < strlen(chrdev_buf); i++){
-		checksum = (checksum *checksum) ^ chrdev_buf[i];
+		checksum = (checksum * checksum) ^ chrdev_buf[i];
 	}
 	
 
@@ -142,23 +142,24 @@ static int ioctl_release(struct inode *inode, struct file *file)
 	return 0;
 }
 
-struct file_operations chrdev_fops = { .owner = THIS_MODULE,
-	.read = chrdev_read,
-	.write = chrdev_write,
-	.open = chrdev_open,
-	.release = chrdev_release
+struct file_operations chrdev_fops = { 
+	.owner 	 	= 	THIS_MODULE,
+	.read		= 	chrdev_read,
+	.write 		= 	chrdev_write,
+	.open 	 	= 	chrdev_open,
+	.release 	= 	chrdev_release
 };
 
 struct file_operations fops = { 
-	.owner = THIS_MODULE,
-	.open = ioctl_open,
-	.unlocked_ioctl = ioctl_ioctl,
-	.release = ioctl_release,
+	.owner          = 	THIS_MODULE,
+	.open           = 	ioctl_open,
+	.unlocked_ioctl = 	ioctl_ioctl,
+	.release 	= 	ioctl_release,
 };
 
 static struct file_operations myops = { 
-	.owner = THIS_MODULE,
-	.read = myread,
+	.owner 		= 	THIS_MODULE,
+	.read 		= 	myread,
 };
 
 static int __init chrdev_init(void)
